77c891f540c5a71fc1621bd289c91030f73eb370
nabeels <nabeel.saabna@whitesourcesoftware.com>
2020-06-08 09:30:22 +0300

    WSE-3381 - Dockerized UA Implementation - phase 1

---
+++ dockerized/wss_agent.bat
@@ -0,0 +1,4 @@
+mkdir wss
+cd wss
+curl -LJO https://github.com/whitesource/unified-agent-distribution/raw/master/standAlone/wss-unified-agent.config
+curl -LJO https://github.com/whitesource/unified-agent-distribution/releases/latest/download/wss-unified-agent.jar
