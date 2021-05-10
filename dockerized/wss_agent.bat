054fd265c3b63483202a024f38000e6889011519
sam quakenbush <sam.quakenbush@whitesource.com>
2021-05-09 20:11:47 -0600

    default config w/ env variables

--- dockerized/wss_agent.bat
+++ dockerized/wss_agent.bat
@@ -1,4 +1,1 @@
-mkdir wss
-cd wss
-curl -LJO https://github.com/whitesource/unified-agent-distribution/raw/master/standAlone/wss-unified-agent.config
 curl -LJO https://github.com/whitesource/unified-agent-distribution/releases/latest/download/wss-unified-agent.jar
